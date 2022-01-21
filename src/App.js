a6b997f7d4f94207962e9445f438aa990c2be632
er-vikram <er.vlipane@gmail.com>
2022-01-21 16:50:54 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,66 +1,70 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 const data = [
   {
     heading: 'Office Map',
-    desc: []
+    desc: [],
   },
   {
     heading: 'New Employee',
-    desc: ['materials','Training']
+    desc: ['materials', 'Training'],
   },
   {
     heading: 'Office Events',
     desc: [
       {
         heading: '2018',
-        desc: ['Summer Picnic','New year party']
-      }
-    ]
-  }
-]
+        desc: [
+          'Summer Picnic',
+          {
+            heading: 'New Employee',
+            desc: ['materials', 'Training'],
+          },
+          'Summer Picnic',
+        ],
+      },
+    ],
+  },
+  {
+    heading: 'Office Map',
+    desc: [],
+  },
+  {
+    heading: 'Office Map',
+    desc: [],
+  },
+];
 
 const isObject = (obj) => {
   return Object.prototype.toString.call(obj) === '[object Object]';
 };
 
 const getNode = (node) => {
-  
-  const desc = node.desc.map((ele)=>{
-
-    if(!isObject(ele)) {
-      return <div className="point">{ele}</div>
+  const desc = node.desc.map((ele) => {
+    if (!isObject(ele)) {
+      return <div className="point">{ele}</div>;
     }
     return getNode(ele);
-  })
+  });
 
-  if(desc.length == 0) {
-    return <div>{node.heading}</div>
+  if (desc.length == 0) {
+    return <div>{node.heading}</div>;
   }
 
   return (
-    <details>
-      <summary>
-        {node.heading}
-      </summary>
-      <div className="description">
-        {desc}
-      </div>
+    <details open>
+      <summary>{node.heading}</summary>
+      <div className="description">{desc}</div>
     </details>
-  )
-}
+  );
+};
 
 export default function App() {
-
-  const comp = data.map((node)=>{
+  const comp = data.map((node) => {
     const details = getNode(node);
     return details;
-  })
+  });
 
-  return (
-    <div>
-      {comp}
-    </div>
-  );
+  return <div>{comp}</div>;
 }
